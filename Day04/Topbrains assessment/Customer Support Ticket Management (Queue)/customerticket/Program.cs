3b32e61e56c37b5ff86cdd14c2d4e3dfbd5f6070
Annu Gill <annugill005@gmail.com>
2026-07-31 15:22:47 +0530

    Assessment 3rd and 4th questions done.

---
+++ Day04/Topbrains assessment/Customer Support Ticket Management (Queue)/customerticket/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] tickets =
+        {
+            "T001|John|Login Issue",
+            "T002|Alice|Payment Failed",
+            "T003|David|Account Locked",
+            "T004|Emma|Refund Request",
+            "T005|James|Password Reset"
+        };
+
+        Queue<string> ticketQueue = new Queue<string>();
+
+        // Enqueue Tickets
+        Console.WriteLine("Enqueued Ticket IDs:");
+        foreach (string ticket in tickets)
+        {
+            ticketQueue.Enqueue(ticket);
+            string[] data = ticket.Split('|');
+            Console.WriteLine(data[0]);
+        }
+
+        // Display All Tickets
+        Console.WriteLine("\nQueue:");
+        foreach (string ticket in ticketQueue)
+        {
+            string[] data = ticket.Split('|');
+            Console.WriteLine($"{data[0]} {data[1]} {data[2]}");
+        }
+
+        // Process First Ticket (Dequeue)
+        Console.WriteLine("\nProcessing Ticket\n");
+        string processedTicket = ticketQueue.Dequeue();
+        string[] processedData = processedTicket.Split('|');
+        Console.WriteLine($"{processedData[0]} {processedData[1]} {processedData[2]}");
+        Console.WriteLine("\nNext Ticket\n");
+
+        // view next ticket
+        string nextTicket = ticketQueue.Peek();
+        string[] nextData = nextTicket.Split('|');
+        Console.WriteLine($"{nextData[0]} {nextData[1]} {nextData[2]}");
+
+        // Check Queue Count
+        Console.WriteLine("\nPending Tickets = " + ticketQueue.Count);
+
+        // search by id
+        Console.WriteLine("\nEnter Ticket ID to search:");
+        string searchId = Console.ReadLine();
+        bool found = false;
+        foreach (string ticket in ticketQueue)
+        {
+            string[] data = ticket.Split('|');
+            if (data[0] == searchId)
+            {
+                Console.WriteLine("\nTicket Found");
+                Console.WriteLine($"Customer : {data[1]}");
+                Console.WriteLine($"Issue : {data[2]}");
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+        {
+            Console.WriteLine("Ticket Not Found");
+        }
+
+        // Count Tickets by Issue Type
+        Console.WriteLine("\nTicket Count by Issue Type\n");
+        int loginCount = 0;
+        int paymentCount = 0;
+        int refundCount = 0;
+        foreach (string ticket in ticketQueue)
+        {
+            string[] data = ticket.Split('|');
+            if (data[2] == "Login Issue")
+                loginCount++;
+            else if (data[2] == "Payment Failed")
+                paymentCount++;
+            else if (data[2] == "Refund Request")
+                refundCount++;
+        }
+        Console.WriteLine($"Login Issue = {loginCount}");
+        Console.WriteLine($"Payment Failed = {paymentCount}");
+        Console.WriteLine($"Refund Request = {refundCount}");
+
+
+        // Remove All Processed Tickets
+        Console.WriteLine("\nRemoving All Processed Tickets\n");
+        while (ticketQueue.Count > 0)
+        {
+            ticketQueue.Dequeue();
+        }
+        Console.WriteLine("Remaining Queue:");
+        if (ticketQueue.Count == 0)
+        {
+            Console.WriteLine("Queue is Empty");
+        }
+        else
+        {
+            foreach (string ticket in ticketQueue)
+            {
+                string[] data = ticket.Split('|');
+                Console.WriteLine($"{data[0]} {data[1]} {data[2]}");
+            }
+        }
+    }
+}
---
+++ Day04/Topbrains assessment/E-Commerce Order Management System/OrderManage/Program.cs
@@ -0,0 +1,100 @@
+﻿// using System;
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         string[] orders =
+//         {
+//             "ORD1001|John Smith|Laptop|2|$1200|Delivered",
+//             "ORD1002|Alice Brown|Mobile|1|$800|Pending",
+//             "ORD1003|David Wilson|Keyboard|3|$150|Shipped",
+//             "ORD1004|Emma Davis|Monitor|2|$350|Delivered",
+//             "ORD1005|James Miller|Mouse|5|$50|Pending"
+//         };
+
+//         // display all order details
+//         foreach (string order in orders)
+//         {
+//             string[] details = order.Split('|');
+
+//             Console.WriteLine("Order ID : " + details[0]);
+//             Console.WriteLine("Customer : " + details[1]);
+//             Console.WriteLine("Product  : " + details[2]);
+//             Console.WriteLine("Quantity : " + details[3]);
+//             Console.WriteLine("Price    : " + details[4]);
+//             Console.WriteLine("Status   : " + details[5]);
+//             Console.WriteLine();
+//         }
+
+//         // convert customer names to upper case
+//         Console.WriteLine("Converted Customer names to upper case");
+//         foreach (string order in orders)
+//         {
+//             string[] details = order.Split('|');
+//             Console.WriteLine(details[1].ToUpper());
+//         }
+//         Console.WriteLine();
+
+//         // display customer initials
+//         Console.WriteLine("Displaying initials of Customer names: ");
+//         foreach (string order in orders)
+//         {
+//             string[] details = order.Split('|');
+//             string[] name = details[1].Split(' ');
+
+//             Console.WriteLine(details[1] + " -> " + name[0][0] + name[1][0]);
+//         }
+//         Console.WriteLine();
+
+//         // display delivered orders
+//         Console.WriteLine("Displaying delivered orders: ");
+//         foreach (string order in orders)
+//         {
+//             if (order.Contains("Delivered"))
+//             {
+//                 string[] details = order.Split('|');
+//                 Console.WriteLine(details[0]);
+//             }
+//         }
+//         Console.WriteLine();
+
+//         // count total orders
+//         Console.WriteLine("Total Orders = " + orders.Length);
+//         Console.WriteLine();
+
+//         //  Search Order by Order ID
+//         Console.Write("Enter Order ID: ");
+//         string orderId = Console.ReadLine();
+
+//         foreach (string order in orders)
+//         {
+//             string[] details = order.Split('|');
+
+//             if (details[0] == orderId)
+//             {
+//                 Console.WriteLine("Customer : " + details[1]);
+//                 Console.WriteLine("Product  : " + details[2]);
+//                 Console.WriteLine("Status   : " + details[5]);
+//                 break;
+//             }
+//         }
+//         Console.WriteLine();
+
+//         // extract price
+//         foreach (string order in orders)
+//         {
+//             string[] details = order.Split('|');
+//             Console.WriteLine(details[4].Replace("$", ""));
+//         }
+
+
+//     }
+// }
+
+
+
+
+
+
+
